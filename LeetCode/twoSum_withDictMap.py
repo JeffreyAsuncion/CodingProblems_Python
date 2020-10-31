af479ada090d88ce9c1e424e522644d82b07a64f
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-31 16:05:23 -0400

    LeetCode twoSum withDictMap

---
+++ LeetCode/twoSum_withDictMap.py
@@ -0,0 +1,58 @@
+"""
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and 
+you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 105
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+"""
+
+arr1 = [1, 3, 5, 6]
+target1 = 8
+
+arr2 = [4, 7, 2, 8]
+target2 = 12
+
+
+def twoSum(nums, target):
+
+    values = dict()
+
+    for index, element in enumerate(nums):
+        
+        comp = target - element
+
+        if comp in values:
+
+            return[values[comp], index]
+        values[element] = index
+    
+    return []
+
+print(twoSum(arr1, target1))
+print(twoSum(arr2, target2))
