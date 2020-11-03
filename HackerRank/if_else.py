01cb4ac43d98d8e8bd21df7aeb396cd4216d0091
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-03 07:24:06 -0500

    HackerRank 1 star

---
+++ HackerRank/if_else.py
@@ -0,0 +1,66 @@
+
+#!/bin/python3
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+    If  is odd, print Weird
+    If  is even and in the inclusive range of 2  to 5 , print Not Weird
+    If  is even and in the inclusive range 6 of 20  to , print Weird
+    If  is even and greater than 20 , print Not Weird
+    Input Format
+
+    A single line containing a positive integer ,n .
+
+    Constraints
+
+    Output Format
+
+    Print Weird if the number is weird. Otherwise, print Not Weird.
+
+    Sample Input 0
+
+    3
+    Sample Output 0
+
+    Weird
+    Explanation 0
+
+     n = 3
+     n is odd and odd numbers are weird, so print Weird.
+
+
+
+     Sample Input 1
+
+     24
+     Sample Output 1
+
+     Not Weird
+
+"""
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+        n = int(input().strip())
+
+        if n < 1:
+            print()
+        elif n % 2 != 0:
+            print("Weird")
+        elif n in range(2, 6):
+            print("Not Weird")
+        elif n in range(6, 21):
+            print("Weird")
+        elif n > 20 and n <= 100:
+            print("Not Weird")
+        else:
+            print()
