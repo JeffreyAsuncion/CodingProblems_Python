a80c5eae8d2df6890851ac4b83056ca1d6ac201b
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-09 06:50:25 -0500

    hr013_Lists.py

--- HackerRank/PythonLanguageProficiency/hr013_Lists.py
+++ HackerRank/PythonLanguageProficiency/hr013_Lists.py
@@ -16,49 +16,84 @@ perform the corresponding operation on your list
 """
 
 def func_insert(lst, i, e):
-    print("\ncalling insert function")
+    #print("\ncalling insert function")
     lst.insert(i, e)
     return lst
 
 def func_print(arg):
-    print("\ncalling print function")
+    #print("\ncalling print function")
     print(arg)
 
 def func_remove(lst, e):
-    print("\ncalling remove function")
     lst.remove(e)
     return lst
 
 def func_append(lst, e):
-    print("\ncalling append function")
+    #print("\ncalling append function")
     lst.append(e)
     return lst
 
 def func_sort(lst):    
-    print("\nCalling Sorting function")
-    lst = sorted(lst) # note the original array is not modified
+    #print("\nCalling Sorting function")
+    lst.sort() # note the original array is modified
     return lst
 
 def func_pop(lst):
-    print("\ncalling Pop Function")
+    #print("\ncalling Pop Function")
     lst.pop()
     return lst
 
 def func_reverse(lst):
-    print("\nCalling Reverse List Function")
-    return lst[::-1]
+    #print("\nCalling Reverse List Function")
+    lst = lst.reverse()
+    return lst
 
-
+# http://openbookproject.net/pybiblio/tips/wilson/dictionaryMenus.php
 if __name__ == '__main__':
+    # read in N num of commands and then commands
     N = int(input())
 
+    raw_command_list = []
+    for _ in range(N):
+        choice = str(input())
+        raw_command_list.append(choice)
+
+    #print(raw_command_list)
+    arr = []
+    commands = []
+    for command in raw_command_list:
+        lst = command.split()
+        commands.append(lst)
+        if lst[0] == "insert":
+            i = int(lst[1])
+            e = int(lst[2])
+            func_insert(arr, i , e)
+        elif lst[0] == "print":
+            func_print(arr)
+        elif lst[0] == "remove":
+            #if arr != []:
+            e = int(lst[1])
+            arr = func_remove(arr, e)   
+        elif lst[0] == "append":
+            e = int(lst[1])
+            func_append(arr, e)
+        elif lst[0] == "sort":
+            func_sort(arr)
+        elif lst[0] == "pop":
+            func_pop(arr)
+        elif lst[0] == "reverse":
+            func_reverse(arr)
+
+    
+    """
     arr = [1, 2, 3]
-    print(arr)
+    print(array)
 
 
     #1. insert i e : Insert integer e at position i
     i = 2
     e =100
+
     func_insert(arr, i, e)
     print(arr)
 
@@ -86,5 +121,5 @@ if __name__ == '__main__':
     print(arr)
 
     #7. reverse: Reverse the list
-
     print(func_reverse(arr))
+    """
