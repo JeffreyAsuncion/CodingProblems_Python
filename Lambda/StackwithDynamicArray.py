199d6010d5a4769fdef0a287d46e8f3717a2577d
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-12-27 22:21:23 -0500

    comments

---
+++ Lambda/StackwithDynamicArray.py
@@ -0,0 +1,17 @@
+# lets make a stack using a dynamic array
+
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        if len(self.data) > 0:
+            return self.data.pop()
+        return "The stack is empty"
+
+    def peek(self):
+        if self.top is not None:
+            return self.top.data
