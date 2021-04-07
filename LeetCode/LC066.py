73d50e8568f67171c398e94d624e5a908fd74611
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-07 14:55:00 -0400

    LC066.py

---
+++ LeetCode/LC066.py
@@ -0,0 +1,60 @@
+"""
+66. Plus One
+Easy
+
+2251
+
+3130
+
+Add to List
+
+Share
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Example 3:
+
+Input: digits = [0]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+Accepted
+812,119
+Submissions
+1,922,754
+
+"""
+def plusOne(digits):
+    ones = digits.pop()
+    if ones < 9:
+        ones += 1
+        digits.append(ones)
+    elif ones == 9:
+        ones = 0
+        tens = digits.pop()
+        tens += 1
+        digits.append(tens)
+        digits.append(ones)
+    return digits
+# need to use linked lists
+
+print(plusOne([1,7,9]))
